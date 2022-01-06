2ea2650ff96dcb2f592559c5c9e1af0db7f8a1be
Alexandra <38242434+Ales12@users.noreply.github.com>
2022-01-06 12:58:16 +0100

    Update avatardatabase.lang.php

--- inc/languages/deutsch_du/avatardatabase.lang.php
+++ inc/languages/deutsch_du/avatardatabase.lang.php
@@ -12,6 +12,7 @@ $l['adb_claim'] = "Avatarperson";
 $l['adb_gender'] = "Geschlecht";
 $l['adb_year'] = "Jahrgang";
 $l['adb_origin'] = "Herkunft";
+$l['adb_hair'] = "Haarfarbe";
 $l['adb_link'] = "Link zu Bildern (Pinterest, Google, etc.)";
 $l['adb_edit'] = "Avatar bearbeiten";
 
@@ -21,3 +22,4 @@ $l['adb_initial_filter'] = "nach Anfangsbuchstaben filtern";
 $l['adb_year_filter'] = "nach Jahrgang filtern";
 $l['adb_origin_filter'] = "nach Herkunft filtern";
 $l['adb_filter'] = "Avatardatenbank Filtern";
+$l['adb_hair_filter'] = "nach Haarfarbe filtern";
